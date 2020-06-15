90311f8b8bb225842ef72898e9a8686ad2692ea8
BryantCabrera <cabrera.bryant@gmail.com>
2020-06-14 19:15:43 -0700

    Creates 1st array problem solution with console.logs() for debugging.

--- arraysAndStringManipulation/mergingMeetingTimes.js
+++ arraysAndStringManipulation/mergingMeetingTimes.js
@@ -37,3 +37,39 @@
 
 // Write a solution that's efficient even when we can't put a nice upper bound on the numbers representing our time ranges. Here we've simplified our times down to the number of 30-minute slots past 9:00 am. But we want the function to work even for very large numbers, like Unix timestamps. In any case, the spirit of the challenge is to merge meetings where startTime and endTime don't have an upper bound.
 
+const mergeRanges = (arr) => {
+	// Sort the array by start times
+	arr.sort(function(a, b) {
+		return a.startTime - b.startTime;
+	});
+	console.log(arr, 'sorted arr');
+
+	const solution = [];
+
+	for (let i = 0; i < arr.length; i++) {
+		console.log(`${i}th iteration starting.`, arr[i].startTime, i === 0 ? solution : solution[solution.length - 1].endTime);
+		// If the current meeting time is the first one or if it starts later than the previous meeting's endTime, keep iterating
+		if (i === 0 || (i !== 0 && arr[i].startTime > solution[solution.length - 1].endTime)) {
+			console.log(i, solution, 'Starts after previous meeting endTime');
+			solution.push(arr[i]);
+			console.log(i, solution, 'Solution after change in > conditional');
+		} else if (arr[i].startTime <= solution[solution.length - 1].endTime) {
+			console.log(i, solution, 'Starts before previous meeting endTime');
+			// If the current meeting range is within the current meeting range, merge the two ranges
+			solution[solution.length - 1].endTime = arr[i].endTime;
+			console.log(i, solution, 'Solution after change in < conditional');
+		}
+	}
+
+	return solution;
+};
+
+const sampleData = [
+  { startTime: 0,  endTime: 1 },
+  { startTime: 3,  endTime: 5 },
+  { startTime: 4,  endTime: 8 },
+  { startTime: 10, endTime: 12 },
+  { startTime: 9,  endTime: 10 },
+];
+
+console.log(mergeRanges(sampleData));
