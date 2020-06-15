d07f5afe711863aeff866dccc4f0eaa5fffd6958
BryantCabrera <cabrera.bryant@gmail.com>
2020-06-14 19:31:40 -0700

    Adds provided solution for reverse string in place problem.

--- arraysAndStringManipulation/reverseStringInPlace.js
+++ arraysAndStringManipulation/reverseStringInPlace.js
@@ -15,3 +15,22 @@ const reverseString = (arr) => {
 const input = ['a', 'b', 'c', 'd'];
 
 console.log(reverseString(input));
+
+//  Provided Solution
+// function reverse(arrayOfChars) {
+
+//   let leftIndex = 0;
+//   let rightIndex = arrayOfChars.length - 1;
+
+//   while (leftIndex < rightIndex) {
+
+//     // Swap characters
+//     const temp = arrayOfChars[leftIndex];
+//     arrayOfChars[leftIndex] = arrayOfChars[rightIndex];
+//     arrayOfChars[rightIndex] = temp;
+
+//     // Move towards middle
+//     leftIndex++;
+//     rightIndex--;
+//   }
+// }
