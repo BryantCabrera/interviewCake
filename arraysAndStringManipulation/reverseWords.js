3bc655fbfb6d6362a0748cb57dda0b613d3d0be8
BryantCabrera <cabrera.bryant@gmail.com>
2020-06-14 19:37:48 -0700

    Scaffolds Reverse Words problem.

--- arraysAndStringManipulation/reverseWords.js
+++ arraysAndStringManipulation/reverseWords.js
@@ -26,3 +26,12 @@
 // When writing your function, assume the message contains only letters and spaces, and all words are separated by one space.
 
 
+const reverseWords = (arr) => {
+
+};
+
+const message = [ 'c', 'a', 'k', 'e', ' ',
+                'p', 'o', 'u', 'n', 'd', ' ',
+								's', 't', 'e', 'a', 'l' ];
+								
+console.log(reverseWords(message));
