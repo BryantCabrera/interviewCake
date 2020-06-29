6a820849dc6e65f1c346ec95411ce69064171b9f
BryantCabrera <cabrera.bryant@gmail.com>
2020-06-28 21:31:20 -0700

    Scaffolds mergeSortedArrays problem.

--- mergeSortedArrays.js
+++ mergeSortedArrays.js
@@ -15,3 +15,15 @@
 // console.log(mergeArrays(myArray, alicesArray));
 // // logs [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19]
 
+const mergeSortedArrays = (arr1, arr2) => {
+	const mergedArrays = [];
+	while (arr1.length || arr2.length) {
+
+	}
+
+	return mergedArrays;
+};
+
+const sampleInput1 = [3, 4, 6, 10, 11, 15];
+const sampleInput2 = [1, 5, 8, 12, 14, 19];
+console.log(mergeSortedArrays());
