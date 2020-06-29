4980cfb76dafdbcc56380f5e26f11249405e1d3e
BryantCabrera <cabrera.bryant@gmail.com>
2020-06-28 22:00:09 -0700

    Creates mergeSortedArrays solution.

--- mergeSortedArrays.js
+++ mergeSortedArrays.js
@@ -18,12 +18,24 @@
 const mergeSortedArrays = (arr1, arr2) => {
 	const mergedArrays = [];
 	while (arr1.length || arr2.length) {
-
+		if (arr1.length && !arr2.length) {
+			mergedArrays.push(arr1.shift())
+		} else if (arr2.length && !arr1.length) {
+			mergedArrays.push(arr2.shift());
+		} else {
+			arr1[0] < arr2[0] ? mergedArrays.push(arr1.shift()) : mergedArrays.push(arr2.shift());
+		}
 	}
 
 	return mergedArrays;
 };
 
 const sampleInput1 = [3, 4, 6, 10, 11, 15];
-const sampleInput2 = [1, 5, 8, 12, 14, 19];
-console.log(mergeSortedArrays());
+const sampleInput2 = [1, 5, 8, 12, 14, 19, 20];
+const sampleInput3 = [1, 5, 8, 12, 14, 19, 20, 21, 23];
+const sampleInput4 = [3, 4, 6, 10, 11, 15, 22];
+const sampleInput5 = [1, 5, 8, 12, 14, 19, 20, 22];
+const sampleInput6 = [1, 5, 8, 12, 14, 19, 20, 22];
+console.log(mergeSortedArrays(sampleInput1, sampleInput2));
+console.log(mergeSortedArrays(sampleInput3, sampleInput4));
+console.log(mergeSortedArrays(sampleInput5, sampleInput6));
